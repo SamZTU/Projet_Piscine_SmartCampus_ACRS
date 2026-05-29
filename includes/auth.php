318a13f7ba1a2493cc5b12ecf8742e675fa10f1c
SamuelKutchukian <samuel.kutchukian@edu.ece.fr>
2026-05-29 14:44:55 +0200

    Authentification

--- includes/auth.php
+++ includes/auth.php
@@ -1,1 +1,22 @@
+<?php
+session_start();
 
+function isLoggedIn() {
+    return isset($_SESSION['user_id']);
+}
+
+function requireLogin() {
+    if (!isLoggedIn()) {
+        header('Location: /smartcampus/index.php');
+        exit();
+    }
+}
+
+function requireRole($role) {
+    requireLogin();
+
+    if ($_SESSION['role'] !== $role) {
+        header('Location: /smartcampus/index.php');
+        exit();
+    }
+}
